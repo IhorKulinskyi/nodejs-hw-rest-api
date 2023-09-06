42648c139f92a08a856612daa8ca39cc145a541a
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-09-06 19:16:46 +0300

    avatars

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 
 const ctrl = require("../../controllers/auth");
 
@@ -15,5 +15,12 @@ router.post("/login", validateBody(loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 
 router.post("/logout", authenticate, ctrl.logOut);
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
+);
 
 module.exports = router;
